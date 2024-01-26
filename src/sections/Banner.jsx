8568cf2fe96f76d1460ec7b64045ad25f2cad1c4
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2024-01-26 14:50:17 +0900

    fixed README and mobile spacing

--- src/sections/Banner.jsx
+++ src/sections/Banner.jsx
@@ -5,7 +5,7 @@ import Icons from "../components/Icons";
 function Banner() {
   return (
     <header className="mx-auto w-full justify-center gap-3 bg-neutral-50 px-12 py-4 shadow-md lg:py-8">
-      <div className="container mx-auto flex max-w-[1100px] flex-col items-center md:flex-row md:justify-between">
+      <div className="container mx-auto flex max-w-[1100px] flex-col items-center gap-2 md:flex-row md:justify-between">
         <Logo />
         <Icons />
       </div>
